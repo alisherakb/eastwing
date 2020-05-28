7b93e5c0a3312540736e71bf93fa6fd3c6762580
Alisher Akbarov <alisher286@gmail.com>
2020-05-29 01:43:09 +0300

    Updates

---
+++ precache-manifest.88269f4d8f86b71d5a0f0f57c152d833.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e2ab3a962d17dae89828f22fb0714f",
+    "url": "/eastwing/index.html"
+  },
+  {
+    "revision": "ab9d61edc98821e9c244",
+    "url": "/eastwing/static/css/2.6e1d1121.chunk.css"
+  },
+  {
+    "revision": "349ab86649e6b04d221e",
+    "url": "/eastwing/static/css/main.84bd1050.chunk.css"
+  },
+  {
+    "revision": "ab9d61edc98821e9c244",
+    "url": "/eastwing/static/js/2.2d83f6c3.chunk.js"
+  },
+  {
+    "revision": "349ab86649e6b04d221e",
+    "url": "/eastwing/static/js/main.debd91fa.chunk.js"
+  },
+  {
+    "revision": "ccb1c9e8a262fa524884",
+    "url": "/eastwing/static/js/runtime~main.99cbd2b6.js"
+  },
+  {
+    "revision": "36064b0eff64943402dc48c0972bafae",
+    "url": "/eastwing/static/media/BG_1.36064b0e.mp4"
+  },
+  {
+    "revision": "558dfd41fe5c55942df674ce1dbe897a",
+    "url": "/eastwing/static/media/BG_2.558dfd41.mp4"
+  },
+  {
+    "revision": "140984fa142938be0aeb1060c90b5dd8",
+    "url": "/eastwing/static/media/charter_services.140984fa.jpeg"
+  },
+  {
+    "revision": "2c098709ee605d2687409fa5aec28c18",
+    "url": "/eastwing/static/media/dispatch.2c098709.jpg"
+  },
+  {
+    "revision": "4a762be9fbae0a29b1f63eb564d35dce",
+    "url": "/eastwing/static/media/fueling.4a762be9.jpg"
+  },
+  {
+    "revision": "64343df95129386ff13817262bb08082",
+    "url": "/eastwing/static/media/ground_handling.64343df9.jpeg"
+  },
+  {
+    "revision": "68527d84bf780063866297baf609d628",
+    "url": "/eastwing/static/media/logo.68527d84.svg"
+  },
+  {
+    "revision": "15d785dffcb374b6ae0bcc1987aa11f7",
+    "url": "/eastwing/static/media/permits.15d785df.jpg"
+  },
+  {
+    "revision": "258ec40d7c3750b13ae922923803c632",
+    "url": "/eastwing/static/media/travel.258ec40d.jpg"
+  }
+]);
